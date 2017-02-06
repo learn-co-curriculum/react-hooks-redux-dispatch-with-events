9c968b5beaa22d447272c64da094a3cc4a65c687
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-06 11:10:22 -0500

    update reducer code

--- reducer.js
+++ reducer.js
@@ -7,21 +7,3 @@ function changeState(state = {count: 0}, action){
         return state;
     }
   }
-
-function dispatch(action){
-  state = changeState(state, action)
-  render()
-}
-
-function render(){
-  let container = document.getElementById('container')
-  container.textContent = state.count
-}
-
-  dispatch({type: '@@INIT'})
-  
-  let button = document.getElementById('button');
-
-  button.addEventListener('click', function(){
-    dispatch({type: 'INCREASE_COUNT'})
-  })
