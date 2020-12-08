83c85fb32050480d3e50848df19ebd601b17dc3f
ihollander <ianholla@gmail.com>
2020-12-07 19:09:04 -0500

    Added solution

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+dispatch({ type: "@@INIT" });
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
